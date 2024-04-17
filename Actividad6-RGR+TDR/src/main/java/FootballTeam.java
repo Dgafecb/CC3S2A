7f93b60c5dbae75e6098eb6fef078cf04434f179
Dgafecb <jdelrosarios@uni.pe>
2024-04-17 09:56:20 -0500

    actividad5+6

---
+++ Actividad6-RGR+TDR/src/main/java/FootballTeam.java
@@ -0,0 +1,11 @@
+public class FootballTeam {
+    //int gamesWon;
+    public FootballTeam(int gamesWon) {
+        //this.gamesWon = gamesWon;
+    }
+
+    public int getGamesWon() {
+
+        return 3;
+    }
+}
