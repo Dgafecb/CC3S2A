0b05168e397e33fc3816688aa919df04e17a8f2f
Dgafecb <jdelrosarios@uni.pe>
2024-04-22 13:10:45 -0500

    implementacion Comparable

--- Actividad6-RGR+TDR/src/test/java/FootballTeamTest.java
+++ Actividad6-RGR+TDR/src/test/java/FootballTeamTest.java
@@ -1,5 +1,9 @@
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 /*
 public class FootballTeamTest {
     @Test
@@ -13,7 +17,50 @@ public class FootballTeamTest {
 }
 */
 public class FootballTeamTest {
+    private static final int ANY_NUMBER = 123;
     private static final int THREE_GAMES_WON = 3;
+    @ParameterizedTest
+    @ValueSource(ints={0,1,3,10})
+    void constructorShouldSetGamesWon(int nbOfGamesWon){
+        FootballTeam team = new FootballTeam(nbOfGamesWon);
+        assertThat(team.getGamesWon())
+                .as("number of games won")
+                .isEqualTo(nbOfGamesWon);
+
+    }
+    @ParameterizedTest
+    @ValueSource(ints={-1,-10})
+    void constructorShouldThrowIllegalArgumentException(int nbOfGamesWon){
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> new FootballTeam(nbOfGamesWon));
+    }
+    @Test
+    void shouldBePossibleToCompareTeams(){
+        FootballTeam team = new FootballTeam(ANY_NUMBER);
+        assertThat(team).isInstanceOf(Comparable.class);
+    }
+    @Test
+    void teamsWithMoreMatchesShouldBeGreater(){
+        FootballTeam team_2 = new FootballTeam(2);
+        FootballTeam team_3 = new FootballTeam(3);
+        assertThat(team_3.compareTo(team_2))
+                .isGreaterThan(0);
+    }
+    @Test
+    void teamsWithLessMatchesShouldBeLesser(){
+        FootballTeam team_2 = new FootballTeam(2);
+        FootballTeam team_3 = new FootballTeam(3);
+        assertThat(team_2.compareTo(team_3))
+                .isLessThan(0);
+    }
+    @Test
+    void teamsWithSameMatchesShouldBeEqual(){
+        FootballTeam teamA = new FootballTeam(2);
+        FootballTeam teamB = new FootballTeam(2);
+        assertThat(teamA.compareTo(teamB))
+                .isEqualTo(0);
+    }
+/*
     @Test
     void constructorShouldSetGamesWon() {
         FootballTeam team = new FootballTeam(THREE_GAMES_WON);
@@ -21,4 +68,5 @@ public class FootballTeamTest {
             .as("number of games won")
             .isEqualTo(THREE_GAMES_WON);
        }
+ */
 }
